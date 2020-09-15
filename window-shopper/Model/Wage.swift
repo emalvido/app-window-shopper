c62ef80ff7eb798fb428fe60e0a1fc0c90a53426
emalvido <emalvido2@hotmail.com>
2020-09-15 18:28:21 +0200

    succesfull unit test

--- window-shopper/Model/Wage.swift
+++ window-shopper/Model/Wage.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Wage {
     class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
-        return Int(round(price / wage))
-//        return Int(ceil(price / wage))
+//        return Int(round(price / wage))
+        return Int(ceil(price / wage))
     }
 }
